9f39f9d60d9ff02a7f5351cdab93d0f6243cf33c
Alex Verbruggen <verbruggenalex@gmail.com>
2017-02-07 18:33:47 +0100

    Added the sniff for checking if custom module is in features directory

--- phpcs/Standards/QualityAssurance/Sniffs/InfoFiles/FeaturesSniff.php
+++ phpcs/Standards/QualityAssurance/Sniffs/InfoFiles/FeaturesSniff.php
@@ -46,20 +46,28 @@ class QualityAssurance_Sniffs_InfoFiles_FeaturesSniff implements PHP_CodeSniffer
         // Only run this sniff once per info file.
         $end = (count($phpcsFile->getTokens()) + 1);
 
-        $fileExtension = strtolower(substr($phpcsFile->getFilename(), -4));
+        $fileName = $phpcsFile->getFilename();
+        $fileExtension = strtolower(substr($fileName, -4));
         if ($fileExtension !== 'info') {
             return $end;
         }
 
         $tokens = $phpcsFile->getTokens();
 
-        $contents = file_get_contents($phpcsFile->getFilename());
+        $contents = file_get_contents($fileName);
         $info     = QualityAssurance_Sniffs_InfoFiles_HelperClass::drupalParseInfoFormat($contents);
+        // Check if the feature is using the latest API.
         if (isset($info['features']['features_api'])
             && in_array('api:1', $info['features']['features_api'])
         ) {
             $warning = 'We highly recommend upgrading features to "api:2"';
             $phpcsFile->addWarning($warning, $stackPtr, 'FeaturesAPI');
+        }
+
+        // Check if it may be just a custom module in the wrong directory.
+        if (!isset($info['features']) && strpos($fileName, '/features/') !== 0) {
+            $error = 'Please move this "custom" module out of the features folder.';
+            $phpcsFile->addError($error, $stackPtr, 'CustomModule');
         }
 
         return $end;
