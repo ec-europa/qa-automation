2788c64697e1c4a31e1e0b69b4cdeeb73555fe55
santosj <joao.santos@ext.ec.europa.eu>
2017-04-04 15:35:13 +0200

    Enforce PSR2 in the file ScanCronCommand.php

--- src/Console/Command/ScanPlatformProvidedCommand.php
+++ src/Console/Command/ScanPlatformProvidedCommand.php
@@ -21,66 +21,65 @@ use Symfony\Component\Console\Style\SymfonyStyle;
  */
 class ScanPlatformProvidedCommand extends Command
 {
-  protected function configure()
-  {
-    $this
-      ->setName('scan:mkpd')
-      ->setDescription('Scan for platform provided modules.')
-      ->addOption('directory', null, InputOption::VALUE_OPTIONAL, 'Path to recursively check.')
-      ->addOption('exclude-dirs', null, InputOption::VALUE_OPTIONAL, 'Directories to exclude.')
-      ->addOption('filename', null, InputOption::VALUE_OPTIONAL, 'Modulename.')
-      ->addOption('profile', null, InputOption::VALUE_OPTIONAL, 'Profile.')
-    ;
-  }
-
-  protected function execute(InputInterface $input, OutputInterface $output)
-  {
-    // Find todos tags.
-    $dirname = !empty($input->getOption('directory')) ? $input->getOption('directory') : getcwd();
-    $exclude_dirs = !empty($input->getOption('exclude-dirs')) ? explode(',', $input->getOption('exclude-dirs')) : NULL;
-    $exclude_dir = is_array($exclude_dirs) ? '--exclude-dir=' . implode(' --exclude-dir=', $exclude_dirs) . ' ' : '';
-    $filename = !empty($input->getOption('filename')) ? $input->getOption('filename') : '@todo';
-    $profile = !empty($input->getOption('profile')) ? $input->getOption('profile') : '@todo';
-
-    if (!empty($filename) && pathinfo($filename, PATHINFO_EXTENSION) !== 'make') {
-      return;
+    protected function configure()
+    {
+        $this
+            ->setName('scan:mkpd')
+            ->setDescription('Scan for platform provided modules.')
+            ->addOption('directory', null, InputOption::VALUE_OPTIONAL, 'Path to recursively check.')
+            ->addOption('exclude-dirs', null, InputOption::VALUE_OPTIONAL, 'Directories to exclude.')
+            ->addOption('filename', null, InputOption::VALUE_OPTIONAL, 'Modulename.')
+            ->addOption('profile', null, InputOption::VALUE_OPTIONAL, 'Profile.');
     }
 
-    // Find site.make in resources folder.
-    $makefile = $filename;
-    $searches = array(
-      'projects' => 'modules or themes',
-      'libraries' => 'libraries',
-    );
-    $duplicates = array();
-    // Get the make file of the profile.
-    $url = 'https://raw.githubusercontent.com/ec-europa/platform-dev/master/resources/' . $profile . '.make';
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HEADER, false);
-    if ($data = curl_exec($curl)) {
-      // Get the contents of the profile make file.
-      $profile = DrupalInfoFormatHelper::drupalParseInfoFormat($data);
-      // Get the contents of the subsite make file.
-      $siteMake = DrupalInfoFormatHelper::drupalParseInfoFormat(file_get_contents($makefile));
-      // Search the subsite make file for duplicates.
-      foreach ($searches as $search => $subject) {
-        // Perform search.
-        if (isset($siteMake[$search])) {
-          foreach ($siteMake[$search] as $name => $contents) {
-            if (isset($profile[$search][$name]) && !isset($siteMake[$search][$name]['patch'])) {
-              $duplicates[$search][] = $name;
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Find todos tags.
+        $dirname = !empty($input->getOption('directory')) ? $input->getOption('directory') : getcwd();
+        $exclude_dirs = !empty($input->getOption('exclude-dirs')) ? explode(',', $input->getOption('exclude-dirs')) : NULL;
+        $exclude_dir = is_array($exclude_dirs) ? '--exclude-dir=' . implode(' --exclude-dir=', $exclude_dirs) . ' ' : '';
+        $filename = !empty($input->getOption('filename')) ? $input->getOption('filename') : '@todo';
+        $profile = !empty($input->getOption('profile')) ? $input->getOption('profile') : '@todo';
+
+        if (!empty($filename) && pathinfo($filename, PATHINFO_EXTENSION) !== 'make') {
+            return;
+        }
+
+        // Find site.make in resources folder.
+        $makefile = $filename;
+        $searches = array(
+            'projects' => 'modules or themes',
+            'libraries' => 'libraries',
+        );
+        $duplicates = array();
+        // Get the make file of the profile.
+        $url = 'https://raw.githubusercontent.com/ec-europa/platform-dev/master/resources/' . $profile . '.make';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        if ($data = curl_exec($curl)) {
+            // Get the contents of the profile make file.
+            $profile = DrupalInfoFormatHelper::drupalParseInfoFormat($data);
+            // Get the contents of the subsite make file.
+            $siteMake = DrupalInfoFormatHelper::drupalParseInfoFormat(file_get_contents($makefile));
+            // Search the subsite make file for duplicates.
+            foreach ($searches as $search => $subject) {
+                // Perform search.
+                if (isset($siteMake[$search])) {
+                    foreach ($siteMake[$search] as $name => $contents) {
+                        if (isset($profile[$search][$name]) && !isset($siteMake[$search][$name]['patch'])) {
+                            $duplicates[$search][] = $name;
+                        }
+                    }
+                }
+                // Print result.
+                if (!empty($duplicates[$search])) {
+                    $output->writeln('<comment>Platform ' . $subject . ' found: </comment><info>' . implode(', ', $duplicates[$search]) . '</info>');
+                    $output->writeln('');
+                }
             }
-          }
         }
-        // Print result.
-        if (!empty($duplicates[$search])) {
-          $output->writeln('<comment>Platform ' . $subject . ' found: </comment><info>' . implode(', ', $duplicates[$search]) . '</info>');
-          $output->writeln('');
-        }
-      }
+        curl_close($curl);
     }
-    curl_close($curl);
-  }
 }
