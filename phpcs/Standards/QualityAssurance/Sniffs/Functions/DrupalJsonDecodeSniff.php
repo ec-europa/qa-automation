07a07a6dace33c5c6d4a270cd46674d277be068b
santosj <joao.santos@ext.ec.europa.eu>
2017-04-10 10:45:12 +0200

    Refactor json-decode sniff.

--- phpcs/Standards/QualityAssurance/Sniffs/Functions/DrupalJsonDecodeSniff.php
+++ phpcs/Standards/QualityAssurance/Sniffs/Functions/DrupalJsonDecodeSniff.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 /**
  * QualityAssurance_Sniffs_Functions_DrupalJsonDecodeSniff.
  *
@@ -9,70 +11,140 @@
  * @package  PHP_CodeSniffer
  * @link     http://pear.php.net/package/PHP_CodeSniffer
  */
-class QualityAssurance_Sniffs_Functions_DrupalJsonDecodeSniff implements PHP_CodeSniffer_Sniff
+class QualityAssurance_Sniffs_Functions_DrupalJsonDecodeSniff extends Drupal_Sniffs_Semantics_FunctionCall
 {
-  /**
-   * Returns an array of tokens this test wants to listen for.
-   *
-   * @return array
-   */
-  public function register()
-  {
-    return array(
-      T_STRING,
-      T_FALSE,
-      T_TRUE
-    );
-  }//end register()
 
-  /**
-   * Processes this test, when one of its tokens is encountered.
-   *
-   * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-   * @param int                  $stackPtr  The position of the current token in the
-   *                                        stack passed in $tokens.
-   *
-   * @return void
-   */
-  public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-  {
-    // Check whole project for string "json_decode".
-    $file_content = file_get_contents($phpcsFile->getFilename());
-    if (strpos($file_content, 'json_decode') === false) {
-      $end = (count($phpcsFile->getTokens()) + 1);
-      return $end;
+    /**
+     * We also want to catch $this->t() calls in Drupal 8.
+     *
+     * @var bool
+     */
+    protected $includeMethodCalls = true;
+
+
+    /**
+     * Returns an array of function names this test wants to listen for.
+     *
+     * @return array
+     */
+    public function registerFunctionNames()
+    {
+        return array(
+            'json_decode',
+            'drupal_json_decode',
+        );
+
+    }//end registerFunctionNames()
+
+
+    /**
+     * Returns start and end token for a given argument number.
+     *
+     * @param int $number
+     * Indicates which argument should be examined, starting with
+     * 1 for the first argument.
+     *
+     * @return array(string => int)
+     */
+    public function getArgument($number)
+    {
+        // Check if we already calculated the tokens for this argument.
+        if (isset($this->arguments[$number]) === true) {
+            return $this->arguments[$number];
+        }
+        $tokens = $this->phpcsFile->getTokens();    // Start token of the first argument.
+        $start = $this->phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens, ($this->openBracket + 1), null, true);
+        if ($start === $this->closeBracket) {
+            // Function call has no arguments, so return false.
+            return false;
+        }
+        // End token of the last argument.
+        $end = $this->phpcsFile->findPrevious(PHP_CodeSniffer_Tokens::$emptyTokens, ($this->closeBracket - 1), null, true);
+        $lastArgEnd = $end;
+        $nextSeperator = $this->openBracket;
+        $counter = 1;
+        while (($nextSeperator = $this->phpcsFile->findNext(T_COMMA, ($nextSeperator + 1), $this->closeBracket)) !== false) {
+            // Make sure the comma belongs directly to this function call,
+            // and is not inside a nested function call or array.
+            $brackets    = $tokens[$nextSeperator]['nested_parenthesis'];
+            $lastBracket = array_pop($brackets);
+            if ($lastBracket !== $this->closeBracket) {
+                continue;
+            }
+            // Update the end token of the current argument.
+            $end =  $this->phpcsFile->findPrevious(PHP_CodeSniffer_Tokens::$emptyTokens, ($nextSeperator - 1), null, true);
+            // Save the calculated findings for the current argument.
+            $this->arguments[$counter] = array(
+                'start' => $start,
+                'end'   => $end,
+            );
+            if ($counter === $number) {
+                break;
+            }
+            $counter++;
+            $start =  $this->phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens, ($nextSeperator + 1), null, true);
+            $end   = $lastArgEnd;
+        }//end while
+        // If the counter did not reach the passed number something is wrong.
+        if ($counter !== $number) {
+            return false;
+        }
+        $this->arguments[$counter] = array(
+            'start' => $start,
+            'end'   => $end,
+        );
+        return $this->arguments[$counter];
     }
 
-    // Get our tokens.
-    $tokens = $phpcsFile->getTokens();
-    $functionHelper = new QualityAssurance_Helper_FunctionUtils($phpcsFile, $stackPtr);
-    $argument = $functionHelper->getArgument(2);
 
-    if ($argument != false) {
-      // Function drupal_json_decode with 2nd parameter is error.
-      if (
-        $tokens[$stackPtr]['content'] == 'drupal_json_decode' &&
-         $argument != false
-      ) {
-        $error = 'The function drupal_json_decode() does not take a 2nd parameter.';
-        $phpcsFile->addError($error, $stackPtr, 'JSONDecode');
-      }
+    /**
+     * Processes this function call.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile    The file being scanned.
+     * @param int                  $stackPtr     The position of the function call in
+     *                                           the stack.
+     * @param int                  $openBracket  The position of the opening
+     *                                           parenthesis in the stack.
+     * @param int                  $closeBracket The position of the closing
+     *                                           parenthesis in the stack.
+     *
+     * @return void
+     */
+    public function processFunctionCall(
+        PHP_CodeSniffer_File $phpcsFile,
+        $stackPtr,
+        $openBracket,
+        $closeBracket
+    ) {
+        $tokens   = $phpcsFile->getTokens();
+        $argument = $this->getArgument(2);
 
-      // Function json_encode only can have T_FALSE as second parameter.
-      if (
-        $tokens[$stackPtr]['content'] == 'json_decode' &&
-        $tokens[$argument['start']]['type'] != 'T_FALSE'
-      ) {
-        $error = 'The function json_decode() is not allowed, use drupal_json_decode() instead.';
-        $phpcsFile->addError($error, $stackPtr, 'JSONDecode');
-      }
-    }
-    else {
-      if ($tokens[$stackPtr]['content'] == 'json_decode') {
-        $error = 'The function json_decode() is not allowed, use drupal_json_decode() instead.';
-        $phpcsFile->addError($error, $stackPtr, 'JSONDecode');
-      }
-    }
-  }//end process()
+        if ($argument != false) {
+            // Function drupal_json_decode with 2nd parameter is error.
+            if (
+                $tokens[$stackPtr]['content'] == 'drupal_json_decode' &&
+                $argument != false
+            ) {
+                $error = 'The function drupal_json_decode() does not take a 2nd parameter.';
+                $phpcsFile->addError($error, $stackPtr, 'JSONDecode');
+            }
+
+            // Function json_encode only can have T_FALSE as second parameter.
+            if (
+                $tokens[$stackPtr]['content'] == 'json_decode' &&
+                $tokens[$argument['start']]['type'] != 'T_FALSE'
+            ) {
+                $error = 'The function json_decode() is not allowed, use drupal_json_decode() instead.';
+                $phpcsFile->addError($error, $stackPtr, 'JSONDecode');
+            }
+        }
+        else {
+            if ($tokens[$stackPtr]['content'] == 'json_decode') {
+                $error = 'The function json_decode() is not allowed, use drupal_json_decode() instead.';
+                $phpcsFile->addError($error, $stackPtr, 'JSONDecode');
+            }
+        }
+    }//end process()
 
 }//end class
+
