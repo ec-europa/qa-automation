125d1a99aa9c8fc1c452b83c0aa0cc8f8dc80ffd
Alex Verbruggen <verbruggenalex@gmail.com>
2019-06-06 08:00:01 +0000

    MULTISITE-21465: First draft.

---
+++ phpcs/QualityAssurance/Test/NamingConventions/ValidVariableNameUnitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace QualityAssurance\Sniffs\NamingConventions;
+
+use QualityAssurance\Test\CoderSniffUnitTest;
+
+class ValidVariableNameUnitTest extends CoderSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList()
+    {
+        return [3 => 1];
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList()
+    {
+        return [];
+
+    }//end getWarningList()
+
+
+}//end class
