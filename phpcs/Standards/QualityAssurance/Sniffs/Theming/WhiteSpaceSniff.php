d4013659a4f6732283bfb5a93d0d5cab0ab4e25c
zarabatana <amgmartins@hotmail.com>
2019-06-12 12:57:37 +0000

    Update test file

--- phpcs/Standards/QualityAssurance/Sniffs/Theming/WhiteSpaceSniff.php
+++ phpcs/Standards/QualityAssurance/Sniffs/Theming/WhiteSpaceSniff.php
@@ -23,13 +23,12 @@ class QualityAssurance_Sniffs_Theming_WhiteSpaceSniff implements PHP_CodeSniffer
    *
    * @return array
    */
-  public function register()
-  {
-    return array(
+    public function register()
+    {
+        return array(
         T_CLOSE_TAG,
-    );
-
-  }//end register()
+        );
+    }//end register()
 
 
   /**
@@ -41,52 +40,50 @@ class QualityAssurance_Sniffs_Theming_WhiteSpaceSniff implements PHP_CodeSniffer
    *
    * @return void
    */
-  public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-  {
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
 
-    // Only run this sniff once per template file.
-    $end = (count($phpcsFile->getTokens()) + 1);
-    // If the file extension is diferent from .tpl.php return.
-    $fileName = $phpcsFile->getFilename();
-    $fileExtension = strtolower(substr($fileName, -8));
-    if ($fileExtension !== '.tpl.php') {
-      return $end;
-    }
+        // Only run this sniff once per template file.
+        $end = (count($phpcsFile->getTokens()) + 1);
+        // If the file extension is diferent from .tpl.php return.
+        $fileName = $phpcsFile->getFilename();
+        $fileExtension = strtolower(substr($fileName, -8));
+        if ($fileExtension !== '.tpl.php') {
+            return $end;
+        }
     
-    // Get our tokens.
-    $tokens = $phpcsFile->getTokens();
-    $token  = $tokens[$stackPtr];
+        // Get our tokens.
+        $tokens = $phpcsFile->getTokens();
+        $token  = $tokens[$stackPtr];
     
-    // Find PHP close tag.    
-    $tagClose = $phpcsFile->findNext(T_CLOSE_TAG, $stackPtr, null);
-    $beforeClosePhp = $tokens[$tagClose - 1];
-    // Get element before whitespace.
-    $tagBeforeWhtsp = $phpcsFile->findPrevious(T_WHITESPACE, $tagClose - 1, null, TRUE);
+        // Find PHP close tag.
+        $tagClose = $phpcsFile->findNext(T_CLOSE_TAG, $stackPtr, null);
+        $beforeClosePhp = $tokens[$tagClose - 1];
+        // Get element before whitespace.
+        $tagBeforeWhtsp = $phpcsFile->findPrevious(T_WHITESPACE, $tagClose - 1, null, true);
 
-    // Check for missing whitespace before closing PHP tag.
-    if ($beforeClosePhp['type'] !== 'T_WHITESPACE') {
-        $error = "Missing white space before closing php tag.";
-        $phpcsFile->addError($error, $stackPtr, 'MissingWhiteSpace', TRUE);
-    }
-    // Check for existent whitespace lenght.
-    if ($beforeClosePhp['type'] === 'T_WHITESPACE' &&
+        // Check for missing whitespace before closing PHP tag.
+        if ($beforeClosePhp['type'] !== 'T_WHITESPACE') {
+            $error = "Missing white space before closing php tag.";
+            $phpcsFile->addError($error, $stackPtr, 'MissingWhiteSpace', true);
+        }
+        // Check for existent whitespace lenght.
+        if ($beforeClosePhp['type'] === 'T_WHITESPACE' &&
         $beforeClosePhp['line'] === $tokens[$tagBeforeWhtsp]['line'] &&
         $beforeClosePhp['length'] > 1) {
-        $error = "There should be just one white space before the closing php tag.";
-        $phpcsFile->addError($error, $stackPtr, 'MultipleWhiteSpaces', TRUE);            
-    }
+            $error = "There should be just one white space before the closing php tag.";
+            $phpcsFile->addError($error, $stackPtr, 'MultipleWhiteSpaces', true);
+        }
 
-    // Check if element 2 positions before closing PHP tag is a colon.
-    $beforeColon = $tokens[$tagClose - 2];
-    if ($beforeColon['type'] === 'T_COLON') {
-      $nonSpace = $tokens[$tagClose - 3];
-      // Check if element 3 positions before closing PHP tag is a whitespace.
-      if ($nonSpace['type'] === 'T_WHITESPACE') {
-        $error = "There shouldn't be any whitespace found before the colon.";
-        $phpcsFile->addError($error, $stackPtr, 'WhiteSpaceFound', TRUE);
-      }
-    }
-
-  }//end process()
-
+        // Check if element 2 positions before closing PHP tag is a colon.
+        $beforeColon = $tokens[$tagClose - 2];
+        if ($beforeColon['type'] === 'T_COLON') {
+            $nonSpace = $tokens[$tagClose - 3];
+            // Check if element 3 positions before closing PHP tag is a whitespace.
+            if ($nonSpace['type'] === 'T_WHITESPACE') {
+                $error = "There shouldn't be any whitespace found before the colon.";
+                $phpcsFile->addError($error, $stackPtr, 'WhiteSpaceFound', true);
+            }
+        }
+    }//end process()
 }//end class
