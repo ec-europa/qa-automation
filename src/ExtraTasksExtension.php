8a9af80aa4ae02dd2a27b0f21040af8b9334b453
João Santos <jonhy81@gmail.com>
2021-06-07 19:32:53 +0200

    DQA-0: Udpate QA Automation.

---
+++ src/ExtraTasksExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace EcEuropa\QaAutomation;
+
+use GrumPHP\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\RuntimeException;
+
+/**
+ * Extension that allows to define extra tasks on local grumphp.yml.dist.
+ */
+class ExtraTasksExtension implements ExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ContainerBuilder $container)
+    {
+        if ($container->hasParameter('extra_tasks')) {
+            $tasks = $container->getParameter('tasks');
+            foreach ($container->getParameter('extra_tasks') as $name => $value) {
+                if (array_key_exists($name, $tasks)) {
+                    throw new RuntimeException("Cannot override already defined task '{$name}' in 'extra_tasks'.");
+                }
+                $tasks[$name] = $value;
+            }
+            $container->setParameter('tasks', $tasks);
+        }
+    }
+}
